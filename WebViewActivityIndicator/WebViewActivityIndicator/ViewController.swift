8142f68ccf160612a6bad35bed164a0a3cafe61c
Marlon David Ruiz Arroyave <mruiz723@gmail.com>
2016-11-15 15:32:08 -0500

    Create project and the scene.

---
+++ WebViewActivityIndicator/WebViewActivityIndicator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WebViewActivityIndicator
+//
+//  Created by Marlon on 15/11/16.
+//  Copyright © 2016 nextu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - IBoutlets
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
